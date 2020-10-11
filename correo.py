5f7a46f298e3a8c0e7e8063af07b6ef9cb7c2f9d
esantiv <72665886+esantiv@users.noreply.github.com>
2020-10-11 14:25:46 +0200

    Update correo.py

--- correo.py
+++ correo.py
@@ -1,3 +1,6 @@
-# Define aquí mismo la función my_email sin argumentos que 
+# Define aquí mismo la función my_email sin argumentos que
+def my_email():
+    return("santiago.escobedo@alu.uclm.es")
 # devuelve tu correo electrónico de la UCLM
+my_email()
 
